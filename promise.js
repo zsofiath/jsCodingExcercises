cec73f301d999da26ba610491248685d311fdb69
zsofi <zsofiath@gmail.com>
2021-12-04 11:59:54 +0100

    :sparkles: constructor and .then() work

--- promise.js
+++ promise.js
@@ -1,13 +1,34 @@
 class MyPromise {
-  constructor() {}
+  resolveValue;
+  resolveFunction = (anyValue) => {
+    this.thenFunction(anyValue);
+  };
+  rejectFunction = () => {};
+  constructor(executor) {
+    executor(this.resolveFunction, this.rejectFunction);
+  }
+  thenFunction;
   static all() {}
   static allSettled(iterable) {}
   static any(iterable) {}
   static race(iterable) {}
   static reject(reason) {}
-  static resolve(value) {}
+  static resolve(value = null) {}
 
   catch() {}
-  then() {}
+  then(thenFunction) {
+    this.thenFunction = thenFunction;
+  }
   finally() {}
 }
+
+// -------------------- Usage -------------------
+let promise = new MyPromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("any value");
+  }, 3000);
+});
+
+promise.then((value) => {
+  console.log(value);
+});
