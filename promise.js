5d557eb61da3fcc682ee210e6ab9ed326d1b0696
zsofi <zsofiath@gmail.com>
2021-12-04 11:33:36 +0100

    :sparkles: create object with signatures

---
+++ promise.js
@@ -0,0 +1,13 @@
+class MyPromise {
+  constructor() {}
+  static all() {}
+  static allSettled(iterable) {}
+  static any(iterable) {}
+  static race(iterable) {}
+  static reject(reason) {}
+  static resolve(value) {}
+
+  catch() {}
+  then() {}
+  finally() {}
+}
